36008a3234c363c91401c18b82ae7899f8ae38e9
agadir001 <110976654+agadir001@users.noreply.github.com>
2023-04-05 00:00:45 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,16 +2,6 @@ variable "agent_count" {
   default = 3
 }
 
-# The following two variable declarations are placeholder references.
-# Set the values for these variable in terraform.tfvars
-variable "aks_service_principal_app_id" {
-  default = ""
-}
-
-variable "aks_service_principal_client_secret" {
-  default = ""
-}
-
 variable "cluster_name" {
   default = "k8stest"
 }
