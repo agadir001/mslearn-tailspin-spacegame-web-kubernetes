9e20b1170a98dd10305e31d32b4f789b05e7a8e4
agadir001 <110976654+agadir001@users.noreply.github.com>
2023-04-05 13:42:11 +0200

    Update acr_variables.tf

--- terraform/acr_variables.tf
+++ terraform/acr_variables.tf
@@ -106,11 +106,6 @@ variable "acr_extra_tags" {
   default     = {}
 }
 
-variable "acr_environment" {
-  description = "Project environment."
-  type        = string
-}
-
 variable "acr_stack" {
   description = "Project stack name."
   type        = string
